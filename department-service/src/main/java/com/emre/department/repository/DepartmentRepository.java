745cc872d9e2bc39f99abb3b3da279e50474e1b2
Şehmus Emre DEMİRALAY <sehmusemre.demiralay@tuikmerkez.tuik.gov.tr>
2023-01-18 11:25:52 +0300

    first commit

---
+++ department-service/src/main/java/com/emre/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.emre.department.repository;
+
+import com.emre.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
